e8d9b6bdb98d96dad220bf200b4e971c01727881
RolyDev <rolyanddev@gmail.com>
2025-01-17 18:47:53 -0500

    init commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/hello", (req, res) => {
+  res.json({ msg: "Hello World!" });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server ready on port ${PORT}`);
+});
+
+module.exports = app;
